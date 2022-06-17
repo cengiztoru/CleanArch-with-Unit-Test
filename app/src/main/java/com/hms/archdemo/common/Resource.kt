7423ad2304a1057c4454ec1ef2b47d5e53a4b262
Cengiz TORU <cengizplyhsp@gmail.com>
2022-06-18 02:28:21 +0300

    Created a State Handler Class

---
+++ app/src/main/java/com/hms/archdemo/common/Resource.kt
@@ -0,0 +1,8 @@
+package com.hms.archdemo.common
+
+
+sealed class Resource<out T>(val data: T? = null, val message: String? = null) {
+    class Success<T>(data: T) : Resource<T>(data)
+    class Error<T>(message: String, data: T? = null) : Resource<T>(data, message)
+    object Loading : Resource<Nothing>()
+}
