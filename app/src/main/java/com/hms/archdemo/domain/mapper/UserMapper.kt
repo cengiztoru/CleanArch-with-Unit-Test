62911ef237168cd00ab4961c7837de5cad5396d4
Cengiz TORU <cengiztoru@gmail.com>
2022-07-17 01:06:56 +0300

    UserRemoteModelMapper renamed as UserMapper

--- app/src/main/java/com/hms/archdemo/domain/mapper/UserMapper.kt
+++ app/src/main/java/com/hms/archdemo/domain/mapper/UserMapper.kt
@@ -5,9 +5,9 @@ import com.hms.archdemo.domain.model.Gender
 import com.hms.archdemo.domain.model.User
 import javax.inject.Inject
 
-class UserRemoteModelMapper @Inject constructor() {
+class UserMapper @Inject constructor() {
 
-    fun mapOnUserRemoteModel(userRemoteModel: UserRemoteModel): User = with(userRemoteModel) {
+    fun mapFrom(userRemoteModel: UserRemoteModel): User = with(userRemoteModel) {
         User(
             fullName = name.first + " " + name.last,
             email = email,
--- app/src/main/java/com/hms/archdemo/domain/use_case/FetchUsersUseCaseImpl.kt
+++ app/src/main/java/com/hms/archdemo/domain/use_case/FetchUsersUseCaseImpl.kt
@@ -2,7 +2,7 @@ package com.hms.archdemo.domain.use_case
 
 import com.hms.archdemo.common.Result
 import com.hms.archdemo.common.extensions.map
-import com.hms.archdemo.domain.mapper.UserRemoteModelMapper
+import com.hms.archdemo.domain.mapper.UserMapper
 import com.hms.archdemo.domain.model.User
 import com.hms.archdemo.domain.repository.UsersRepository
 import kotlinx.coroutines.flow.Flow
@@ -11,7 +11,7 @@ import javax.inject.Inject
 
 class FetchUsersUseCaseImpl @Inject constructor(
     private val usersRepository: UsersRepository,
-    private val userRemoteModelMapper: UserRemoteModelMapper,
+    private val userMapper: UserMapper,
     // you can use multiple repositories in UseCase classes
     // you can use other UseCase classes, too
 ) : FetchUsersUseCase {
@@ -19,7 +19,7 @@ class FetchUsersUseCaseImpl @Inject constructor(
     override suspend fun fetchUsers(): Flow<Result<List<User>>> {
         return usersRepository.fetchUsers().map {
             it.map { userRemoteModelList ->
-                userRemoteModelList.map(userRemoteModelMapper::mapOnUserRemoteModel)
+                userRemoteModelList.map(userMapper::mapFrom)
             }
         }
     }
