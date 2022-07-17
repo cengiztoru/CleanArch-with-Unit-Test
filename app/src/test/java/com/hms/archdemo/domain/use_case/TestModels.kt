84cf4917b5f4235d4547e3a9866853c1d51b9912
Cengiz TORU <cengiztoru@gmail.com>
2022-07-17 22:28:36 +0300

    Implemented test of UserViewModelTest success use case

--- app/src/test/java/com/hms/archdemo/domain/use_case/TestModels.kt
+++ app/src/test/java/com/hms/archdemo/domain/use_case/TestModels.kt
@@ -2,6 +2,7 @@ package com.hms.archdemo.domain.use_case
 
 import com.hms.archdemo.domain.mapper.UserFactory
 import com.hms.archdemo.domain.model.Gender
+import com.hms.archdemo.ui.users.UserItemUiState
 
 fun maleUser() = UserFactory.createUser(Gender.MALE)
 fun femaleUser() = UserFactory.createUser(Gender.FEMALE)
@@ -11,3 +12,5 @@ fun fakeUserList() = listOf(
     femaleUser(),
     undefinedUser()
 )
+
+fun fakeUserListItemsWithUiState() = fakeUserList().map { user -> UserItemUiState(user) }
--- app/src/test/java/com/hms/archdemo/ui/users/UserViewModelTest.kt
+++ app/src/test/java/com/hms/archdemo/ui/users/UserViewModelTest.kt
@@ -1,7 +1,9 @@
 package com.hms.archdemo.ui.users
 
 import com.hms.archdemo.domain.use_case.FakeFetchUsersUseCase
+import com.hms.archdemo.domain.use_case.fakeUserListItemsWithUiState
 import com.hms.archdemo.util.CoroutineRule
+import com.hms.archdemo.util.extensions.shouldBe
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Assert.assertTrue
 import org.junit.Rule
@@ -22,6 +24,28 @@ class UserViewModelTest {
 
         //Then
         assertTrue(viewModel.uiState.isLoading)
+    }
+
+    @Test
+    fun `Given fetching users successfully, When getUsers called, Then uiState should update as success`() {
+
+        //Given
+        val fetchUsersUseCase = FakeFetchUsersUseCase(isSuccessful = true)
+        val viewModel = UserViewModel(fetchUsersUseCase)
+
+        val expectedUiState = UserListUiState(
+            isLoading = false,
+            fakeUserListItemsWithUiState(),
+            error = ""
+        )
+
+        //When
+        coroutineRule.testDispatcher.scheduler.runCurrent()
+        val actualUiState = viewModel.uiState
+
+        //Then
+        actualUiState shouldBe expectedUiState
+
     }
 
 }
